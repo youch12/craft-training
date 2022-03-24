69fda5b4b2a4db27f28987652064d7553d61fcb4
Yousra Mouho <ymouho@octo.com>
2022-03-24 09:55:58 +0100

    Proxy pattern end

--- src/main/java/ma/awb/craft/ImageProxy.java
+++ src/main/java/ma/awb/craft/ImageProxy.java
@@ -2,7 +2,17 @@ package ma.awb.craft;
 
 public class ImageProxy implements Image {
 
-  public void display() {
+  private RealImage realImage;
+  private String fileName;
 
+  public ImageProxy(String fileName) {
+    this.fileName = fileName;
+  }
+
+  public void display() {
+    if (realImage == null) {
+      realImage = new RealImage(fileName);
+    }
+    realImage.display();
   }
 }
--- src/main/java/ma/awb/craft/Main.java
+++ src/main/java/ma/awb/craft/Main.java
@@ -3,5 +3,9 @@ package ma.awb.craft;
 public class Main {
 
   public static void main(String[] args) {
+    Image image = new ImageProxy("awb_banner.jpg");
+
+    image.display();
+    image.display();
   }
 }
